08200f00d05928090cdd4b705856e365a0f7843b
paritytech-ci <devops-team@parity.io>
2022-03-21 12:40:49 +0000

    Updated docs for master

--- polkadot_approval_distribution/sidebar-items.js
+++ polkadot_approval_distribution/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["ApprovalDistribution","The Approval Distribution subsystem."],["Metrics","Approval Distribution metrics."]]});
+initSidebarItems({"struct":[["ApprovalDistribution","The Approval Distribution subsystem."]]});
