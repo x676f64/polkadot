0a95ebc14ce7a1c447ba7d8d64d208271ca073bc
paritytech-ci <devops-team@parity.io>
2022-04-28 18:33:15 +0000

    Updated docs for master

---
+++ implementors/pallet_nomination_pools/pallet/trait.Config.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["westend_runtime"] = [{"text":"impl <a class=\"trait\" href=\"pallet_nomination_pools/pallet/trait.Config.html\" title=\"trait pallet_nomination_pools::pallet::Config\">Config</a> for <a class=\"struct\" href=\"westend_runtime/struct.Runtime.html\" title=\"struct westend_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["westend_runtime::Runtime"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
