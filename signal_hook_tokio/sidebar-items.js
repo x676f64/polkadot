76e346596c83b0b48e97b8054cdd1a5bf896bf26
paritytech-ci <devops-team@parity.io>
2022-07-19 07:50:53 +0000

    Updated docs for master

---
+++ signal_hook_tokio/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Handle","A struct to control an instance of an associated type (like for example [`Signals`][super::Signals])."],["SignalsInfo","An asynchronous [`Stream`] of arriving signals."]],"type":[["Signals","Simplified version of the signals stream."]]});
